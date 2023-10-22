b6c478b754cbec39500bf93291114abec16a1801
archanabai <121252186+archanabai@users.noreply.github.com>
2023-10-22 16:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const button = document.getElementById("enterBtn");
+const ptag =document.getElementById("status");
+function onclickfun(){
+	ptag.innerHTML ="<h1>Entered Metaverse</h1>";
+}
